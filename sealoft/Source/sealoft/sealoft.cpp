8cf8936706f00ef1cd7f450da40f28abf09fcc19
Luca Belluso <lucabelluso123@gmail.com>
2023-08-22 15:40:45 +0200

    Imported project

---
+++ sealoft/Source/sealoft/sealoft.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "sealoft.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, sealoft, "sealoft" );
